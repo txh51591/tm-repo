76d663ab95f7b5723917a418bc7f6a9a62bd0ac1
John McDonald <john@threatrix.io>
2020-11-17 07:31:46 -0800

    Update URLUtil.java

--- src/main/java/io/threatrix/threatmatrix/utility/springboot/URLUtil.java
+++ src/main/java/io/threatrix/threatmatrix/utility/springboot/URLUtil.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class URLUtil {
 
