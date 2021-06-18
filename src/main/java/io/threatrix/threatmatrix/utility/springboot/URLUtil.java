f4a61a5454498a265ff43ae6e36b17b89da45a6d
John McDonald <john@threatrix.io>
2021-06-18 08:52:40 -0700

    Update URLUtil.java

--- src/main/java/io/threatrix/threatmatrix/utility/springboot/URLUtil.java
+++ src/main/java/io/threatrix/threatmatrix/utility/springboot/URLUtil.java
@@ -24,8 +24,7 @@ public class URLUtil {
         *      copyright 2012-2019 the original author or authors
         *    
         *    Licenses:
-        *      Server Side Public License, v 1
-        *      xa0
+        *      Apache 2.0
         *    
         *    Auto-attribution by Threatrix, Inc.
         *    
