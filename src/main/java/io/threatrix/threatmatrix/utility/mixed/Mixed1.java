4eacd10c185911301b6892d7378317a5f3cfd08b
John McDonald <john@threatrix.io>
2021-08-01 17:34:06 -0700

    Added JmsInvokerClientInterceptor.java:v5.0.4.RELEASE to Mixed1.java

--- src/main/java/io/threatrix/threatmatrix/utility/mixed/Mixed1.java
+++ src/main/java/io/threatrix/threatmatrix/utility/mixed/Mixed1.java
@@ -165,7 +165,7 @@ public class Mixed1 {
     /*
        github@spring-projects/spring-framework:PathMatchingBenchmark.java:v5.3.9
        modifications: none
-   */
+    */
     static class RouteGenerator {
 
         static List<Route> staticRoutes() {
@@ -237,7 +237,28 @@ public class Mixed1 {
 
     }
 
+    /*
+        github@spring-projects/spring-framework:JmsInvokerClientInterceptor.java:v5.0.4.RELEASE
+        modifications: none
+    */
+    protected RemoteInvocationResult executeRequest(RemoteInvocation invocation) throws JMSException {
+        Connection con = createConnection();
+        Session session = null;
+        try {
+            session = createSession(con);
+            Queue queueToUse = resolveQueue(session);
+            Message requestMessage = createRequestMessage(session, invocation);
+            con.start();
+            Message responseMessage = doExecuteRequest(session, queueToUse, requestMessage);
+            if (responseMessage != null) {
+                return extractInvocationResult(responseMessage);
+            } else {
+                return onReceiveTimeout(invocation);
+            }
+        } finally {
+            JmsUtils.closeSession(session);
+            ConnectionFactoryUtils.releaseConnection(con, getConnectionFactory(), true);
+        }
 
-
-
+    }
 }
